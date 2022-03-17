b9af8b0fcf9b793a127be33ee4ab57327a231fcd
Dan Souza <souza.music@outlook.com>
2022-03-17 14:13:21 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let allDogs = []
+let badDogs = []
 
 document.addEventListener('DOMContentLoaded', () => {
     getDogs()
@@ -18,6 +19,7 @@ function getDogs() {
 function dogBar(doggo) {
     const dogBar = document.getElementById('dog-bar')
     const dogInf = document.getElementById("dog-info")
+    const dogFilter = document.querySelector("#good-dog-filter")
     const img = document.createElement('IMG')
     img.className = 'pic'
     img.src = `${doggo.image}`
@@ -25,13 +27,32 @@ function dogBar(doggo) {
     span.dataset.id = doggo.id
     span.innerText = `${doggo.name}`
     const btn = document.createElement("button")
-    const gob = `${doggo.isGoodDog}`
+    let gob = `${doggo.isGoodDog}`
+    
+
+    dogFilter.addEventListener("click", (e) => {
+        let currentVal = e.target.innerText.includes("OFF")
+            ? off : on
+        if (currentVal) {
+            e.target.innerText = "Filter good dogs: ON";
+            
+    }
+        else {
+            e.target.innerText = "Filter good dogs: OFF"
+
+        }
+
+    })
+
 
     if (gob === "true") {
         btn.innerText = ("Good Dog");
     }
     else if (gob === "false") {
-        btn.innerText = ("Bad Dog");
+        btn.innerText = ("Bad Dog")
+        badDogs = gob
+        
+        
     }
     else NaN
 
@@ -40,33 +61,38 @@ function dogBar(doggo) {
     })
     span.addEventListener("click", (e) => {
         dogInf.append(span.innerText)
-        dogInf.append()
         dogInf.append(img)
         dogInf.append(btn)
 
         btn.addEventListener("click", (e) => {
-            if(gob === "true")
-                btn.innerText = "Bad Dog"
-            else if(gob === "false")
-                btn.innerText = "Good Dog"
+            let currentVal = e.target.innerText.includes("Good")
+                ? true : false
+            if (currentVal) {
+                e.target.innerText = "Bad Dog"
+            }
+            else {
+                e.target.innerText = "Good Dog"
+            }
 
-                fetch(`http://localhost:3000/pups/`+doggo.id, {
-                    method: "PATCH",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json"
-                    },
-                    body: JSON.stringify(gob)
-                })
+            fetch(`http://localhost:3000/pups/` + doggo.id, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify({ isGoodDog: !currentVal })
+            })
                 .then(response => response.json())
                 .then(data => {
-                    console.log("success", data)
+                    console.log(data)
+
                 })
                 .catch((error) => {
                     console.error("error", error)
                 })
 
         })
+
     })
     dogBar.append(span)
 }
